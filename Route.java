d332f04a7c9e67d81576c25cc3d99583eecd422f
madihighs <113549967+madihighs@users.noreply.github.com>
2023-02-07 22:23:51 -0800

    2/7

--- Route.java
+++ Route.java
@@ -1,2 +1,13 @@
 public class Route {
+    private int start_point;
+    private int end_point;
+    private int number;
+    private String name;
+
+    private static void reRoute(){}
+    private static void addStop(){}
+    private static void routeProgress(){}
+    private static void eta(){}
+    public static void main(String[] args){}
+
 }
